96383797b3a2a628b114bb8351b36d4983c0813d
Sebastian Zarnekow <sebastian.zarnekow@gmail.com>
2022-10-01 17:10:33 +0200

    Remove unnecessary file

--- 1-mvn.sh
+++
@@ -1,9 +0,0 @@
-mvn \
-            -f org.eclipse.xtext.maven.parent/pom.xml \
-            --batch-mode \
-            --update-snapshots \
-            -fae \
-            -PuseJenkinsSnapshots \
-            -Dmaven.test.failure.ignore=true \
-            -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn \
-            clean verify
